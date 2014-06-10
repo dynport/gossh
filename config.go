c5f6df8604d8a3f48d6ab067a0952127015e5c9f
Tobias Schwab <tobias.schwab@dynport.de>
2014-06-10 19:10:36 +0200

    add config type to get raw ssh connections (from password and/or agent)

---
+++ config.go
@@ -0,0 +1,37 @@
+package gossh
+
+import (
+	"fmt"
+	"net"
+	"os"
+
+	"code.google.com/p/go.crypto/ssh"
+	"code.google.com/p/go.crypto/ssh/agent"
+)
+
+type Config struct {
+	Host     string
+	User     string
+	Port     int
+	Password string
+}
+
+func (c *Config) Connection() (*ssh.Client, error) {
+	port := c.Port
+	if port == 0 {
+		port = 22
+	}
+
+	var auths []ssh.AuthMethod
+	if c.Password != "" {
+		auths = append(auths, ssh.Password(c.Password))
+	} else if sshAgent, e := net.Dial("unix", os.Getenv("SSH_AUTH_SOCK")); e == nil {
+		auths = append(auths, ssh.PublicKeysCallback(agent.NewClient(sshAgent).Signers))
+	}
+
+	config := &ssh.ClientConfig{
+		User: c.User,
+		Auth: auths,
+	}
+	return ssh.Dial("tcp", fmt.Sprintf("%s:%d", c.Host, port), config)
+}
